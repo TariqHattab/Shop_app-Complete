7e22d4fa728be009aaeaa456d88d67d738eb3904
TariqHattab <trqzh123@gmail.com>
2021-03-28 12:22:32 +0300

    commit 2

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/providers/cart.dart';
 
